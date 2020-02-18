5a143efbf6351ff79b6d5a8d4d0e67f59cd455ce
Luis Lucana <luislucana@gmail.com>
2020-02-18 00:22:57 -0300

    Criação do swagger-codegen para gerar os Controllers.

---
+++ target/generated-sources/swagger/src/gen/java/main/io/swagger/api/PetApiController.java
@@ -0,0 +1,32 @@
+package io.swagger.api;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.stereotype.Controller;
+import javax.servlet.http.HttpServletRequest;
+import java.util.Optional;
+@javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2020-02-18T00:16:25.767-03:00")
+
+@Controller
+public class PetApiController implements PetApi {
+
+    private final ObjectMapper objectMapper;
+
+    private final HttpServletRequest request;
+
+    @org.springframework.beans.factory.annotation.Autowired
+    public PetApiController(ObjectMapper objectMapper, HttpServletRequest request) {
+        this.objectMapper = objectMapper;
+        this.request = request;
+    }
+
+    @Override
+    public Optional<ObjectMapper> getObjectMapper() {
+        return Optional.ofNullable(objectMapper);
+    }
+
+    @Override
+    public Optional<HttpServletRequest> getRequest() {
+        return Optional.ofNullable(request);
+    }
+
+}
