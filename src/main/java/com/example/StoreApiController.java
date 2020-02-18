c55f6000fcc0a83e6199caa453ff18bbe628c393
Luis Lucana <luislucana@gmail.com>
2020-02-18 10:27:53 -0300

    Correção do pom.xml para gerar o codigo-fonte na pasta correta.

---
+++ src/main/java/com/example/StoreApiController.java
@@ -0,0 +1,32 @@
+package com.example;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.stereotype.Controller;
+import javax.servlet.http.HttpServletRequest;
+import java.util.Optional;
+@javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2020-02-18T10:26:09.946-03:00")
+
+@Controller
+public class StoreApiController implements StoreApi {
+
+    private final ObjectMapper objectMapper;
+
+    private final HttpServletRequest request;
+
+    @org.springframework.beans.factory.annotation.Autowired
+    public StoreApiController(ObjectMapper objectMapper, HttpServletRequest request) {
+        this.objectMapper = objectMapper;
+        this.request = request;
+    }
+
+    @Override
+    public Optional<ObjectMapper> getObjectMapper() {
+        return Optional.ofNullable(objectMapper);
+    }
+
+    @Override
+    public Optional<HttpServletRequest> getRequest() {
+        return Optional.ofNullable(request);
+    }
+
+}
